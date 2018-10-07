6898354d531750fc7ce590b2d91277e5a2df25ed
annaboyatyuk <annaboyatyuk@gmail.com>
2018-10-07 00:07:04 -0700

    routes

--- src/auth/router.js
+++ src/auth/router.js
@@ -5,6 +5,8 @@ const authRouter = express.Router();
 
 import User from './user.js';
 import auth from './auth.js';
+import oauth from './oauth.js';
+import Profile from './profile.js';
 
 
 authRouter.get('/', (req, res) => {
@@ -25,21 +27,45 @@ authRouter.post('/signup', (req, res, next) => {
 });
 
 authRouter.get('/signin', auth, (req, res, next) => {
-  res.cookie('Token', req.token);
+  res.cookie('token', req.token);
   res.send(req.token);
 });
 
 
+authRouter.get('/oauth/google/code',(req, res, next) => {
+  oauth.googleAuthorize(req)
+    .then(token => {
+      res.cookie('token', token);
+      res.redirect(`${process.env.API_URL}/signin`);
+    })
+    .catch(next);
+});
 
 
 
-// let sendJSON = (res, data) => {
-//   res.statusCode = 200;
-//   res.statusMessage = 'OK';
-//   res.setHeader('Content-Type', 'application/json');
-//   res.write(JSON.stringify(data));
-//   res.end();
-// };
+authRouter.get('/api/v1/profiles', (req, res, next) => {
+  Profile.find({}, {_id: 0, userID: 0})
+    .then(profiles => {
+      res.send(profiles);
+    })
+    .catch(err => {
+      next(err);
+    });
+});
+
+authRouter.get('/api/v1/profiles/id/:id', (req, res) => {
+  Profile.findOne({_id: req.params.id}, {_id: 0, userID: 0})
+    .then(profile => {
+      res.send(profile);
+    });
+});
+
+authRouter.get('/api/v1/profiles/username/:id', (req, res) => {
+  Profile.findOne({username: req.params.id}, {_id: 0, userID: 0})
+    .then(profile => {
+      res.send(profile);
+    });
+});
 
 
 export default authRouter;
